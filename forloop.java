22109d49485f259dc223336130c5a95b96fe9a6f
IshaanGaba <ishaangaba14@gmail.com>
2023-06-25 00:34:39 +0530

    kdjgdg

--- forloop.java
+++ forloop.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class forloop {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        int c=1;
         
         sc.close();
     }
