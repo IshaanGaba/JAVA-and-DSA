6d667f171a6de7cb8f978f9c7832215d257cc494
IshaanGaba <ishaangaba14@gmail.com>
2023-06-28 02:22:31 +0530

    adv

---
+++ patternadv.java
@@ -0,0 +1,11 @@
+public class patternadv {
+    public static void inverted_rorated_half_pyramid(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+            }
+        }
+    }
+    public static void main(String args[]) {
+        
+    }
+}
