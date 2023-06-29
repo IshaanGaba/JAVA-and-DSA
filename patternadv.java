a9ee1d15633e78d0987e43b0a841ae9cccb91887
IshaanGaba <ishaangaba14@gmail.com>
2023-06-29 23:56:28 +0530

    adv

--- patternadv.java
+++ patternadv.java
@@ -1,11 +1,42 @@
 public class patternadv {
-    public static void inverted_rorated_half_pyramid(int n) {
-        for(int i=1;i<=n;i++){
+    public static void hollow_rectangle(int m,int n) {
+        for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
+                if(i==1 || i==m|| j==1| j==n){
+                    System.out.print('*');
+                }
+                else{
+                    System.out.print(' ');
+                }
             }
+            System.out.println();
+        }
+
+    }
+    public static void inverted_rotated_half_pyramid(int n) {
+        for(int i=1;i<=n;i++){
+            //spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(' ');
+            }
+            //star
+            for(int j=1;j<=i;j++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+    public static void inverted_half_pyramid_withnos(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print(j);
+            }
+            System.out.println();
         }
     }
     public static void main(String args[]) {
-        
+        // hollow_rectangle(5,4);
+        // inverted_rotated_half_pyramid(5);
+        inverted_half_pyramid_withnos(5);
     }
 }
