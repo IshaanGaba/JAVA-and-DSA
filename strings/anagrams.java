020e55de9c4850bb6b4bc560663d2e1b11638ba2
IshaanGaba <ishaangaba14@gmail.com>
2023-07-05 17:35:35 +0530

    string

---
+++ strings/anagrams.java
@@ -0,0 +1,33 @@
+package strings;
+
+import java.util.Arrays;
+
+// An anagram is a word or phrase formed by rearranging the letters of another word or phrase, typically using all the original letters exactly once. For example, the word anagram itself can be rearranged into nag a ram, as well as the word binary into brainy and the word adobe into abode
+public class anagrams {
+    public static int anagram(String s, String d) {
+        if(s.length()!=d.length()){
+            return -1; 
+        }
+        s=s.toLowerCase();
+        d=d.toLowerCase();
+        char sa[]=s.toCharArray();
+        char da[]=d.toCharArray();
+
+        Arrays.sort(sa);
+        Arrays.sort(da);
+        boolean result=Arrays.equals(sa,da);
+        if(result){
+            return 1;
+        }
+        return -1;
+
+    }
+    public static void main(String[] args) {
+        String s="shaan";
+        String d="ishaan";
+        System.out.println(anagram(s, d));
+        
+
+    }
+    
+}
