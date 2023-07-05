83ef0067894dbc9d41d900d61a8f141479b7750d
IshaanGaba <ishaangaba14@gmail.com>
2023-07-05 14:07:17 +0530

    strings

---
+++ strings/pallindrome.java
@@ -0,0 +1,20 @@
+package strings;
+
+import java.util.Scanner;
+
+public class pallindrome {
+    public static boolean ispallindrome(String s) {
+        for(int i=0;i<s.length()/2;i++){
+            if(s.charAt(i)!=s.charAt(s.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(ispallindrome(s));
+        sc.close();
+    }
+}
