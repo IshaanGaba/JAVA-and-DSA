a43990175e81021a49338b5fc56ba29348bc82c4
IshaanGaba <ishaangaba14@gmail.com>
2023-07-09 01:19:09 +0530

    bit

---
+++ bit_manipulation/oddoreven.java
@@ -0,0 +1,16 @@
+package bit_manipulation;
+
+public class oddoreven {
+    public static String oe(int a) {
+        if((a&1) !=1){//the last digit of odd is always 1 and even is 0 check eg
+            return "even"; 
+        }
+        return "odd"; 
+    }
+    public static void main(String[] args) {
+        System.out.println(oe(101));
+        System.out.println(oe(100));
+        System.out.println(oe(1001));
+        System.out.println(oe(1010));
+    }
+}
