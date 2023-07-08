fd00c5ff530f4affb4970de618985934957d1fe3
IshaanGaba <ishaangaba14@gmail.com>
2023-07-09 01:10:26 +0530

    bit

--- bit_manipulation/binaryleft.java
+++ bit_manipulation/binaryleft.java
@@ -3,7 +3,7 @@ package bit_manipulation;
 public class binaryleft {
     public static void main(String[] args) {
         System.out.println(5<<2);
-        /* see the pointed edges for left or right
+        /* see the pointed edges for left or right(a<<b)-->a*2^(b)
         here the pointed edge is towards left
          * 5-000101
          * now shift whole to 2 places left and add zero to other place
