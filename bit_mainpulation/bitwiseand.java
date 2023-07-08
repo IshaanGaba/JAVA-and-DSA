787812adc5e984c00e96925b7f571073e0076e9b
IshaanGaba <ishaangaba14@gmail.com>
2023-07-08 21:00:26 +0530

    bit

---
+++ bit_mainpulation/bitwiseand.java
@@ -0,0 +1,15 @@
+package bit_mainpulation;
+
+public class bitwiseand{
+    public static void main(String[] args) {
+        System.out.println(5 & 6);
+        /*5--0101 6--0110
+          1&1-1
+          1&0-0
+          0&1-0
+          0&0-0
+          100-->4
+         */
+    }
+}
+
---
+++ bit_mainpulation/bitwiseor.java
@@ -0,0 +1,14 @@
+package bit_mainpulation;
+
+public class bitwiseor {
+    public static void main(String[] args) {
+        System.out.println(5 | 6);
+        /*5--0101 6--0110
+          1&1-1
+          1&0-1
+          0&1-1
+          0&0-0
+          111-->7
+         */
+    }
+}
