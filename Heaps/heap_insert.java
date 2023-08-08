9a01a92fdebb673281626a7c89a902eba7a023de
IshaanGaba <ishaangaba14@gmail.com>
2023-08-08 15:45:26 +0530

    heap

--- Heaps/heap_insert.java
+++ Heaps/heap_insert.java
@@ -1,7 +1,40 @@
 package Heaps;
 
+import java.util.ArrayList;
+
 public class heap_insert {
+    public static void insert(int data,ArrayList<Integer>ans) {
+        if(ans.isEmpty()){
+            ans.add(data);
+            return;
+        }
+        ans.add(data);
+        int i=ans.size()-1;
+        int par=(i-1)/2;
+        while(ans.get(i)<ans.get(par)){//min heap
+        // while(ans.get(i)>ans.get(par)){//max heap
+            int temp=ans.get(i);
+            ans.set(i, ans.get(par));
+            ans.set(par, temp);
+            i=par;
+            par=(i-1)/2;
+
+        }
+
+    }
     public static void main(String[] args) {
-        
+        ArrayList<Integer> ans=new ArrayList<>();
+        ArrayList<Integer> ans1=new ArrayList<>();
+        ans.add(2);
+        ans.add(3);
+        ans.add(4);
+        ans.add(5);
+        ans.add(10);
+        ans.add(1);
+        System.out.println(ans);
+        for (int i = 0; i < ans.size(); i++) {
+            insert(ans.get(i), ans1);
+        }
+        System.out.println(ans1);
     }
 }
--- Heaps/priority_queue.java
+++ Heaps/priority_queue.java
@@ -1,5 +1,40 @@
 package Heaps;
 
+import java.util.PriorityQueue;
+
 public class priority_queue {
-    
+    static class Student implements Comparable<Student>{
+        int indx;
+        int rank;
+        public Student(int indx,int rank){
+            this.indx=indx;
+            this.rank=rank;
+        }
+
+        @Override
+        public int compareTo(Student s1){
+            return this.rank-s1.rank;
+        }
+    }
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        PriorityQueue<Student> pq1=new PriorityQueue<>();
+        pq.add(5);
+        pq.add(2);
+        pq.add(3);
+        pq.add(1);
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek()+" ");
+            pq.remove();
+        }
+        System.out.println();
+        pq1.add(new Student(1, 5));
+        pq1.add(new Student(2, 1));
+        pq1.add(new Student(3, 25));
+        pq1.add(new Student(4, 3));
+        while (!pq1.isEmpty()) {
+            System.out.println(pq1.peek().indx+" ");
+            pq1.remove();
+        }
+    }
 }
