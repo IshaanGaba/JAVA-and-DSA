306e221d37cb1623b81817f15ce4c5b121caf0e9
IshaanGaba <ishaangaba14@gmail.com>
2023-08-20 12:43:47 +0530

    graphs3

---
+++ Graphs3/dijkstra_algo.java
@@ -0,0 +1,84 @@
+package Graphs3;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class dijkstra_algo{
+    static class edge{
+        int src;
+        int des;
+        int wt;
+        public edge(int src,int des,int wt){
+            this.src=src;
+            this.des=des;
+            this.wt=wt;
+        }
+    }
+    static class pair implements Comparable<pair>{
+        int n;
+        int path;
+        public pair(int n,int path){
+            this.n=n;
+            this.path=path;
+        }
+        @Override
+        public int compareTo(pair p1){
+            return this.path-p1.path;
+        }
+    }
+    public static void create(ArrayList<edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i]=new ArrayList<>();
+        }
+        graph[0].add(new edge(0, 1, 2));
+        graph[0].add(new edge(0, 2, 4));
+
+        graph[1].add(new edge(1, 3, 7));
+        graph[1].add(new edge(1, 2, 1));
+
+        graph[2].add(new edge(2, 4, 3));
+
+        graph[3].add(new edge(3, 5, 1));
+
+        graph[4].add(new edge(4, 3, 2));
+        graph[4].add(new edge(4, 5, 5));
+    }
+    public static void dijkstra(ArrayList<edge>[] graph,int src) {
+        int dis[]=new int[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if(i!=src){
+                dis[i]=Integer.MAX_VALUE;
+            }
+        }
+        boolean vis[]=new boolean[graph.length];
+        PriorityQueue<pair> pq=new PriorityQueue<>();
+        pq.add(new pair(src, 0));
+        while (!pq.isEmpty()) {
+            pair curr=pq.remove();
+            if(!vis[curr.n]){
+                vis[curr.n]=true;      
+                for (int i = 0; i < graph[curr.n].size(); i++) {
+                    edge e=graph[curr.n].get(i);
+                    int u=e.src;
+                    int v=e.des;
+                    int wt=e.wt;
+                    if(dis[u]+wt<dis[v]){
+                        dis[v]=dis[u]+wt;
+                        pq.add(new pair(v, dis[v]));
+                    }
+                }
+            }
+        }
+        
+        for (int i = 0; i < dis.length; i++) {
+            System.out.println(dis[i]);
+        }
+    }
+    public static void main(String[] args) {
+        int v=6;
+        @SuppressWarnings("unchecked")
+        ArrayList<edge>[] graph=new ArrayList[v];
+        create(graph);
+        dijkstra(graph,0);
+    }
+}
