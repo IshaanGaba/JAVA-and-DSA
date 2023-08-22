39e4a9541814a7ee0a8814a9f0d36678a69e2367
IshaanGaba <ishaangaba14@gmail.com>
2023-08-22 13:25:58 +0530

    graphs4

---
+++ Graphs4/MST_prim_algo.java
@@ -0,0 +1,73 @@
+package Graphs4;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+//for undirected weighted graph
+public class MST_prim_algo {
+    static class edge{
+        int src;
+        int des;
+        int wt;
+        public edge(int src,int des,int wt){
+            this.src=src;
+            this.des=des;
+            this.wt=wt;
+        }
+    }
+    public static void create(ArrayList<edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i]=new ArrayList<>();
+        }
+        graph[0].add(new edge(0, 1, 10));
+        graph[0].add(new edge(0, 2, 15));
+        graph[0].add(new edge(0, 3, 30));
+
+        graph[1].add(new edge(1, 0, 10));
+        graph[1].add(new edge(1, 3, 40));
+
+        graph[2].add(new edge(2, 0, 15));
+        graph[2].add(new edge(2, 3, 50));
+
+        graph[3].add(new edge(3, 0, 30));
+        graph[3].add(new edge(3, 1, 40));
+        graph[3].add(new edge(3, 2, 50));
+    }
+    static class Pair implements Comparable<Pair>{
+        int v;
+        int cost;
+        public Pair(int v,int cost){
+            this.v=v;
+            this.cost=cost;
+        }
+
+        @Override
+        public int compareTo(Pair p2){
+            return this.cost-p2.cost;
+        }
+    }
+    public static void prim(ArrayList<edge>[] graph) {
+        PriorityQueue<Pair> pq=new PriorityQueue<>();
+        boolean vis[]=new boolean[graph.length];
+        pq.add(new Pair(0, 0));
+        int finalcost=0;
+        while(!pq.isEmpty()){
+            Pair curr=pq.remove();
+            if(!vis[curr.v]){
+                vis[curr.v]=true;
+                finalcost+=curr.cost;
+                for (int i = 0; i < graph[curr.v].size(); i++) {
+                    edge e=graph[curr.v].get(i);
+                    pq.add(new Pair(e.des, e.wt));
+                }
+            }
+        }
+        System.out.println(finalcost);
+    }
+    public static void main(String[] args) {
+        int v=5;
+        @SuppressWarnings("unchecked")
+        ArrayList<edge>[] graph=new ArrayList[v];
+        create(graph);
+        prim(graph);
+    }
+}
--- Graphs4/bellman_ford_algo.java
+++ Graphs4/bellman_ford_algo.java
@@ -1,40 +1,61 @@
 package Graphs4;
 
 import java.util.*;
-
+//dijkstra for -ve edges-------O(V*E)
 public class bellman_ford_algo {
     static class edge{
         int src;
         int des;
-        public edge(int src,int des){
+        int wt;
+        public edge(int src,int des,int wt){
             this.src=src;
             this.des=des;
+            this.wt=wt;
         }
     }
     public static void create(ArrayList<edge>[] graph) {
         for (int i = 0; i < graph.length; i++) {
             graph[i]=new ArrayList<>();
         }
+        graph[0].add(new edge(0, 1, 2));
+        graph[0].add(new edge(0, 2, 4));
 
-        graph[0].add(new edge(0, 1));
-        graph[0].add(new edge(0, 2));
-        
-        graph[1].add(new edge(1, 2));
-        
-        graph[2].add(new edge(2, 0));
-        graph[2].add(new edge(2, 1));
+        graph[1].add(new edge(1, 2, -4));
 
-        // graph[0].add(new edge(0, 1));
-        
-        // graph[1].add(new edge(1, 0));
-        // graph[1].add(new edge(1 , 2));
+        graph[2].add(new edge(2, 3, 2));
+
+        graph[3].add(new edge(3, 4, 4));
+
+        graph[4].add(new edge(4, 1, -1));
     }
-    
+    public static void bellman(ArrayList<edge>[] graph,int src ) {
+        int v=graph.length;
+        int dis[]=new int[v];
+        for (int i = 0; i < dis.length; i++) {
+            if(i!=src){
+                dis[i]=Integer.MAX_VALUE;
+            }
+        }
+        for (int i = 0; i < v-1; i++) {//u->v how many vertices from u=v-1
+            //edges
+            for (int j = 0; j < graph.length; j++) {
+                for (int k = 0; k < graph[j].size(); k++) {
+                    edge e =graph[j].get(k);
+                    if(dis[e.src]!=Integer.MAX_VALUE &&dis[e.src]+e.wt<dis[e.des]){
+                        dis[e.des]=dis[e.src]+e.wt;
+                    }
+                }
+            }
+        } 
+        for (int i = 0; i < dis.length; i++) {
+            System.out.println(dis[i]);
+        }
+    }
     public static void main(String[] args) {
-        int v=4;
+        int v=5;
         @SuppressWarnings("unchecked")
         ArrayList<edge>[] graph=new ArrayList[v];
         create(graph);
-        
+        bellman(graph, 0);
     }
 }
