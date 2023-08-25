8cae4ec2ea5f0ddc22e290d058ef01035c34cf1c
IshaanGaba <ishaangaba14@gmail.com>
2023-08-25 23:39:05 +0530

    graphs5

---
+++ Graphs5/connecting_cities_mincost.java
@@ -0,0 +1,45 @@
+package Graphs5;
+//if matrix is given,in edge class use des,wt only .dont use src as many edges do not exist.and if we store in graph,wt-0 will be stored and least will be 0.
+import java.util.PriorityQueue;
+
+public class connecting_cities_mincost{
+    static class edge implements Comparable<edge>{
+        
+        int des;
+        int wt;
+        public edge(int des,int wt){
+            this.des=des;
+            this.wt=wt;
+        }
+        @Override
+        public int compareTo(edge p2){
+            return this.wt-p2.wt;
+        }
+    }
+
+    public static int mincost(int cities[][]) {
+        int v=cities.length;
+        int min=0;
+        PriorityQueue<edge> pq=new PriorityQueue<>();
+        pq.add(new edge(0, 0));
+        boolean vis[]=new boolean[v];
+        while(!pq.isEmpty()){
+            edge curr=pq.remove();
+            if(!vis[curr.des]){
+                vis[curr.des]=true;
+                min+=curr.wt;
+                for (int i = 0; i < cities[curr.des].length; i++) {
+                    if(cities[curr.des][i]!=0){//edge does not exist
+                        pq.add(new edge(i,cities[curr.des][i]));
+                    }
+                }
+            } 
+        } 
+        return min;
+    }
+    public static void main(String[] args) {
+        int cities[][]={{0,1,2,3,4},{1,0,5,0,7},{2,5,0,6,0},{3,0,6,0,0},{4,7,0,0,0}};
+        System.out.println(mincost(cities));
+
+    }
+}
