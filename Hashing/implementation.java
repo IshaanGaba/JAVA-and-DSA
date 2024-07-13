bdc0e3523efea93589a77dc5093b6ac73d42c3dc
IshaanGaba <ishaangaba14@gmail.com>
2024-07-13 15:44:06 +0530

    imp

---
+++ Hashing/implementation.java
@@ -0,0 +1,153 @@
+package Hashing;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class implementation {
+    static class HashMap<K, V> {
+        private class Node {
+            K key;
+            V value;
+
+            public Node(K k, V v) {
+                this.key = k;
+                this.value = v;
+            }
+        }
+
+        private int n;// n
+        private int N;// n
+        private LinkedList<Node> buckets[];// N
+        @SuppressWarnings("unchecked")
+        public HashMap() {
+            this.N = 4;
+            this.buckets = new LinkedList[4];
+            for (int i = 0; i < 4; i++) {
+                this.buckets[i] = new LinkedList<>();
+            }
+        }
+
+        private int hashFunction(K key) {
+            int hc = key.hashCode();
+            return Math.abs(hc) % buckets.length;
+        }
+
+        private int searchInLL(K key, int bi) {
+            LinkedList<Node> ll = buckets[bi];
+            int di = 0;
+            for (int i = 0; i < ll.size(); i++) {
+                Node node = ll.get(i);
+                if (node.key == key) {
+                    return di;
+                }
+                di++;
+            }
+            return -1;
+        }
+        @SuppressWarnings("unchecked")
+        private void rehash() {
+            LinkedList<Node> oldbuck[] = buckets;
+            buckets = new LinkedList[2 * N];
+            N = 2 * N;
+            for (int i = 0; i < buckets.length; i++) {
+                buckets[i] = new LinkedList<>();
+            }
+
+            for (int i = 0; i < oldbuck.length; i++) {
+                LinkedList<Node> ll = oldbuck[i];
+                for (int j = 0; j < ll.size(); j++) {
+                    Node node = ll.remove();
+                    put(node.key, node.value);
+                }
+            }
+        }
+
+        public boolean containsKey(K key) {
+            int bi = hashFunction(key);
+            int di = searchInLL(key, bi);
+
+            if (di != -1) {
+                return true;
+
+            } else {
+                return false;
+            }
+        }
+
+        public V get(K key) {
+            int bi = hashFunction(key);
+            int di = searchInLL(key, bi);
+
+            if (di != -1) {
+                Node node = buckets[bi].get(di);
+                return node.value;
+
+            } else {
+                return null;
+            }
+        }
+
+        public V remove(K key) {
+            int bi = hashFunction(key);
+            int di = searchInLL(key, bi);
+
+            if (di != -1) {
+                Node node = buckets[bi].remove(di);
+                n--;
+                return node.value;
+
+            } else {
+                return null;
+            }
+        }
+
+        public ArrayList<K> keySet() {
+            ArrayList<K> keys = new ArrayList<>();
+            for (int i = 0; i < buckets.length; i++) {
+                LinkedList<Node> ll = buckets[i];
+                for (int j = 0; j < ll.size(); j++) {
+                    Node node = ll.get(j);
+                    keys.add(node.key);
+                }
+            }
+            return keys;
+        }
+
+        public boolean isEmpty() {
+            return n == 0;
+        }
+
+        public void put(K key, V val) {
+            int bi = hashFunction(key);
+            int di = searchInLL(key, bi);
+
+            if (di != -1) {
+                Node node = buckets[bi].get(di);
+                node.value = val;
+
+            } else {
+                buckets[bi].add(new Node(key, val));
+                n++;
+            }
+            double lambda = n / N;
+            if (lambda > 2.0) {
+                rehash();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("india", 100);
+        map.put("china", 50);
+        map.put("pak", 150);
+        ArrayList<String> k = map.keySet();
+        System.out.println(k);
+        for (String i:map.keySet()) {
+            System.out.println(map.get(i));
+        }
+        System.out.println(map.remove("india"));
+
+    }
+
+}
